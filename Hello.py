e221e4689e20a6049d059675854ed036794ad6fb
Ashish Kumar <104843117+ashish12ak@users.noreply.github.com>
2024-04-06 12:11:13 +0000

    modify

--- Hello.py
+++ Hello.py
@@ -13,39 +13,75 @@
 # limitations under the License.
 
 import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
+import time
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    
 
 
-if __name__ == "__main__":
-    run()
+
+with st.sidebar:
+    st.title("Retrieval of Clinical Trials")
+    choice = st.radio("Navigation", ["Sparse Retrieval", "Dense Retrieval"], index=0)
+    st.info("This application allows you to retrieve most relevant clinical trials based on your query")
+
+background_image = """
+<style>
+[data-testid="stAppViewContainer"] > .main {
+  background-image: url("https://images.unsplash.com/photo-1614850523011-8f49ffc73908?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+  background-position: center;  
+  background-repeat: no-repeat;
+}
+</style>
+"""
+st.markdown(background_image, unsafe_allow_html=True)
+
+def typewriter(text: str, speed: int):
+    tokens = text.split()
+    container = st.empty()
+    for index in range(len(tokens) + 1):
+        curr_full_text = " ".join(tokens[:index])
+        container.markdown(curr_full_text)
+        time.sleep(1 / speed)
+
+def convert_df(df):
+    return df.to_csv(index=False).encode('utf-8')
+  
+
+if choice == 'Sparse Retrieval' : 
+
+
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+    background-size: cover;
+    }
+    </style>
+    '''
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    
+
+    text = "Welcome to the interface for Sparse Retrieval !"
+    speed = 10
+    typewriter(text=text, speed=speed)
+
+    text_1 = "Type your query in the box and you can see the most relevant clinical trials in the sidebar."
+
+    speed_1 = 10
+    typewriter(text=text_1, speed=speed_1)
+
+    
+
+
+
+
+
+
+
+    
+
+
